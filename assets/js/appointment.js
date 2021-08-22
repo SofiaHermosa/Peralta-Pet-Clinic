b7ac87846bbc7c0f65503c987d1fa6f803dcff17
Joshua Blando <jblando@htechcorp.net>
2021-08-22 19:30:31 +0800

    ##Changes## added custom css for appointment calendar

--- assets/js/appointment.js
+++ assets/js/appointment.js
@@ -68,16 +68,16 @@ document.addEventListener('DOMContentLoaded', function() {
       $('body').toggleClass('overflow-hidden');
     },
     eventContent: function(info) {
-      console.log();
+      console.log(info);
       return {
       'html' : `<div data-aos="flip-up" data-aos-duration="1000" class="w-full">
-                    <div class="font-semibold text-md text-center opacity-0 sm:opacity-100">Sample User</div>
-                      <div class="text-sm font-light mt-1 px-2 opacity-0 sm:opacity-100">
-                        <ul class="list-outside">
-                            <li>Grooming - <span class="text-gray-500">06:00 AM</span></li>
-                            <li>Vaccine - <span class="text-gray-500">07:00 AM</span></li>
-                        </ul>
-                    </div>
+                    <div class="font-bold text-md text-left text-blue-500 opacity-0 sm:opacity-100">Sample User</div>
+                      <div class="text-sm text-gray-500 font-semibold text-justify overflow-hidden overflow-ellipsis font-light mt-1 px-2 opacity-0 sm:opacity-100">
+                        ${info.event._def.title}
+                      </div>
+                      <div class="w-full text-xs py-1 text-blue-400 text-right">
+                          ${info.timeText}m
+                      </div>
                   </div>`,
       };
     }
