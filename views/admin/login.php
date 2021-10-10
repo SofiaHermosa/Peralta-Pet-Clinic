03246a237613608cd952a2c3c78d262f4a040c9f
sofia <hermosasofia1118@gmail.com>
2021-10-10 11:00:48 -0700

    ##Changes## fixed access levels of users

--- views/admin/login.php
+++ views/admin/login.php
@@ -26,13 +26,13 @@
 </head>
 <body>
 	<div class="bg-gray-100 w-full h-screen">
-		<img src="<?php echo '../../'.$branding->images[0]  ?? '../../../assets/images/logo.png' ?>" class="w-28 fixed top-28 left-1/2 transform -translate-x-1/2" alt="">
-		<div class="w-2/6 h-96 p-10 mt-6 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+		<img src="<?php echo '../../'.$branding->images[0]  ?? '../../../assets/images/logo.png' ?>" class="w-28 fixed top-24 left-1/2 transform -translate-x-1/2" alt="">
+		<div class="w-2/6 h-96 p-10 mt-2 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
 			<form action="/login/1" method="POST">
 			<div class="grid grid-cols-1 gap-6">
 				<div class="col-span-1 text-center mb-4">
-					<h1 class="text-xl font-extrabold text-indigo-700"><?php echo $branding->comp_name ?? 'Peralta Dog and Cat Clinic' ?></h1>
-					<h1 class="text-md mt-2 font-bold text-gray-500 block">Login as Administrator</h1>
+					<h1 class="text-2xl font-extrabold text-indigo-700"><?php echo $branding->comp_name ?? 'Peralta Dog and Cat Clinic' ?></h1>
+					<h1 class="text-md font-bold text-gray-500 block">Login as Administrator</h1>
 				</div>
 				<div class="col-span-1">
 					<label class="form_label">Email</label>
@@ -44,7 +44,7 @@
 					<input type="password" name="password" class="form_input bg-white bg-opacity-60 focus:ring-indigo-100 focus:border-indigo-100 duration-300" id="password">
 				</div>
 
-				<div class="col-span-1">
+				<div class="col-span-1 mt-4">
 					<button type="submit" class="p-2 mt-4 mb-20 w-full bg-indigo-700 text-white font-bold rounded-md shadow-md hover:bg-indigo-500">Login</button>
                 </div>
 
